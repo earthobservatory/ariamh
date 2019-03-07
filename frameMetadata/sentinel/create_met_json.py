7e6d0bb83c847a8e9a8373ddcd198c47b7b089c6
shitong01 <stchin@ntu.edu.sg>
2019-03-07 19:29:07 +0800

    Changes to ariamh to allow sling-extract to run for opendataset (#5)
    * add ingets job as aria job because gdal
    
    * correct malformed json file
    
    * chmod for python to run as executable
    
    * individual submission type for opds testing since we dont use context
    
    * make localize_urls in context
    
    * remove unnecessary line
    
    * ensure passes provenance without localize_url available
    
    * better indentation for metadata
    
    * correct spli_swath+products to use product-id instead of zip_file to name metadata files
    
    * ensure split_swath_products.py uses prod_id to name metadata/browse/dataset files
    
    * ensure we use original zip file as the archive_filename. Not assume that met.json has same basename as archive file.
    
    * remove unneeded files

--- frameMetadata/sentinel/create_met_json.py
+++ frameMetadata/sentinel/create_met_json.py
@@ -3,7 +3,7 @@ import os, sys, json, re
 from lxml.etree import parse
 
 
-def create_met_json(xml_file, json_file, mis_char):
+def create_met_json(xml_file, json_file, zip_file, mis_char):
     """Write product metadata json."""
 
     with open(xml_file) as f:
@@ -30,7 +30,7 @@ def create_met_json(xml_file, json_file, mis_char):
     direction = doc.xpath("//*[local-name() = 'pass']")[0].text
     if direction == "ASCENDING": direction = "asc"
     else: direction = "dsc"
-    archive_filename = os.path.basename(json_file).replace('.met.json', '.zip')
+    archive_filename = zip_file
     metadata = {
         "archive_filename": archive_filename,
         "bbox": bbox,
